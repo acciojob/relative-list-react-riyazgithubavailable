431b51421c395e52ade40ed74aa25f8ca1bd247b
riyazgithubavailable <riyazisheikh97@gmail.com>
2024-02-27 15:44:23 +0530

    relativeAssign

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const App = () => {
+  const relative = [
+    'John',
+    'Alice',
+    'Bob',
+    'Emily'
+  ]
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                 {
+                  relative.map((data,index)=>(
+                    <li key={`relativeListItem${index + 1}`}>
+                      {data}
+                    </li>
+                  ))
+                 }
+               </ol>
+
     </div>
   )
 }
