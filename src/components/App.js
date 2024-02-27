cfd6b2c2461d75399a1ae02fb65dbf6a8b00e7bb
riyazgithubavailable <riyazisheikh97@gmail.com>
2024-02-27 15:55:12 +0530

    updateRelative

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React from 'react'
 
 const App = () => {
   const relative = [
-    'John',
-    'Alice',
-    'Bob',
-    'Emily'
+    {
+      key:'relativeListItem1',
+      name:'John'
+    },
+    {
+      key:'relativeListItem2',
+      name:'Alice'
+    },
+    
+    {
+      key:'relativeListItem3',
+      name:'Bob'
+    }
+    
   ]
   return (
     <div id="main">
                {/* Do not remove the main div */}
                <ol key="relativeList">
                  {
-                  relative.map((data,index)=>(
-                    <li key={`relativeListItem${index + 1}`}>
-                      {data}
+                  relative.map((data)=>(
+                    <li key={data.key}>
+                      {data.name}
                     </li>
                   ))
                  }
